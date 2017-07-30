f0a0af0de36c4c7d561428477e046131a93e1f26
Shresth <vermashresth@gmail.com>
2017-07-30 19:51:42 +0530

    changes

--- style.py
+++ style.py
@@ -116,3 +116,99 @@ class StyleLoss(nn.Module):
     def backward(self, retain_variables=True):
         self.loss.backward(retain_variables=retain_variables)
         return self.loss
+
+
+
+cnn = models.vgg19(pretrained=True).features
+
+# move it to the GPU if possible:
+if use_cuda:
+    cnn = cnn.cuda()
+
+print (cnn)
+
+# desired depth layers to compute style/content losses :
+content_layers_default = ['conv_4']
+style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+
+def get_style_model_and_losses(cnn, style_img, content_img,
+                               style_weight=1000, content_weight=1,
+                               content_layers=content_layers_default,
+                               style_layers=style_layers_default):
+    cnn = copy.deepcopy(cnn)
+
+    # just in order to have an iterable access to or list of content/syle
+    # losses
+    content_losses = []
+    style_losses = []
+
+    model = nn.Sequential()  # the new Sequential module network
+    gram = GramMatrix()  # we need a gram module in order to compute style targets
+
+    # move these modules to the GPU if possible:
+    if use_cuda:
+        model = model.cuda()
+        gram = gram.cuda()
+
+    i = 1
+    for layer in list(cnn):
+        if isinstance(layer, nn.Conv2d):
+            name = "conv_" + str(i)
+            model.add_module(name, layer)
+
+            if name in content_layers:
+                # add content loss:
+                target = model(content_img).clone()
+                content_loss = ContentLoss(target, content_weight)
+                model.add_module("content_loss_" + str(i), content_loss)
+                content_losses.append(content_loss)
+
+            if name in style_layers:
+                # add style loss:
+                target_feature = model(style_img).clone()
+                target_feature_gram = gram(target_feature)
+                style_loss = StyleLoss(target_feature_gram, style_weight)
+                model.add_module("style_loss_" + str(i), style_loss)
+                style_losses.append(style_loss)
+
+        if isinstance(layer, nn.ReLU):
+            name = "relu_" + str(i)
+            model.add_module(name, layer)
+
+            if name in content_layers:
+                # add content loss:
+                target = model(content_img).clone()
+                content_loss = ContentLoss(target, content_weight)
+                model.add_module("content_loss_" + str(i), content_loss)
+                content_losses.append(content_loss)
+
+            if name in style_layers:
+                # add style loss:
+                target_feature = model(style_img).clone()
+                target_feature_gram = gram(target_feature)
+                style_loss = StyleLoss(target_feature_gram, style_weight)
+                model.add_module("style_loss_" + str(i), style_loss)
+                style_losses.append(style_loss)
+
+            i += 1
+
+        if isinstance(layer, nn.MaxPool2d):
+            name = "pool_" + str(i)
+            model.add_module(name, layer)  # ***
+
+    return model, style_losses, content_losses
+
+input_img = content_img.clone()
+# if you want to use a white noise instead uncomment the below line:
+# input_img = Variable(torch.randn(content_img.data.size())).type(dtype)
+
+# add the original input image to the figure:
+plt.figure()
+imshow(input_img.data, title='Input Image')
+
+def get_input_param_optimizer(input_img):
+    # this line to show that input is a parameter that requires a gradient
+    input_param = nn.Parameter(input_img.data)
+    optimizer = optim.LBFGS([input_param])
+    return input_param, optimizer
